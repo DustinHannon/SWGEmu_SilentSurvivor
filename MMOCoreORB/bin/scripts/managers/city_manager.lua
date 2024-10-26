962927fb3bd91e9ca311fff945f61013bdf6bbaf
Dustin Hannon <126910524+DustinHannon@users.noreply.github.com>
2024-10-26 17:53:31 -0500

    Update city_manager.lua
    Changed city maintenance discount to 50% of normal.

--- MMOCoreORB/bin/scripts/managers/city_manager.lua
+++ MMOCoreORB/bin/scripts/managers/city_manager.lua
@@ -85,7 +85,7 @@ TrainersPerRank = 5
 MissionTerminalsPerRank = 5
 
 -- Amount to discount city maintenance  float.  1 = 100%, .75 =75%, .5=50% etc
-maintenanceDiscount = 1.0
+maintenanceDiscount = .5
 
 --[[
 	CITIES ALLOWED PER PLANET
