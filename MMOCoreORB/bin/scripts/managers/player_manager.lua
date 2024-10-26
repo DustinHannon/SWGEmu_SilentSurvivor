77bb38903ec34a0c41e5ec188db85526f8a237b7
Dustin Hannon <126910524+DustinHannon@users.noreply.github.com>
2024-10-26 17:38:27 -0500

    Update player_manager.lua
    Increased the duration and power of the blue frog buffs

--- MMOCoreORB/bin/scripts/managers/player_manager.lua
+++ MMOCoreORB/bin/scripts/managers/player_manager.lua
@@ -48,10 +48,10 @@ onlineCharactersPerAccount = 1 --How many characters are allowed online from a s
 allowSameAccountPvpRatingCredit = 0
 
 --Blue frog / GM buff values
-performanceBuff = 1100
-medicalBuff = 2200
-performanceDuration = 7200 -- in seconds
-medicalDuration = 7200 -- in seconds
+performanceBuff = 2200
+medicalBuff = 4400
+performanceDuration = 17200 -- in seconds
+medicalDuration = 17200 -- in seconds
 
 --Sets the experience multiplier while grouped
 groupExpMultiplier = 1.2
