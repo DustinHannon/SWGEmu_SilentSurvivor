f8a2a37dbe5e2f9622344e708f5b099e36bdcfb6
Dustin Hannon <126910524+DustinHannon@users.noreply.github.com>
2024-10-26 17:58:09 -0500

    Update planet_manager.lua
    Changed Shuttleports and Starports to have shorter away times, and to stay longer once landed.

--- MMOCoreORB/bin/scripts/managers/planet/planet_manager.lua
+++ MMOCoreORB/bin/scripts/managers/planet/planet_manager.lua
@@ -41,16 +41,16 @@
 --which carries forward this exception.
 
 --Time, in seconds, that a shuttle is away for
-shuttleportAwayTime = 300
-starportAwayTime = 60
+shuttleportAwayTime = 30
+starportAwayTime = 30
 
 --Time, in seconds, that a shuttle takes to land
-shuttleportLandingTime = 11
-starportLandingTime = 14
+shuttleportLandingTime = 5
+starportLandingTime = 8
 
 --Time, in seconds, that a shuttle is landed for
-shuttleportLandedTime = 120
-starportLandedTime = 120
+shuttleportLandedTime = 160
+starportLandedTime = 160
 
 
 corellia = {
